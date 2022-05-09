37dac990af46ddd873a64b1c7803051afd56733f
= <=>
2022-05-09 12:24:15 -0500

    Paso 8: Agregando Endpoint Delete para eliminar un explorer

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.put('/v1/explorers/:id',(req,res)=>{
     res.status(200).json({message: "Update!"})
 })
 
+app.delete('/v1/explorers/:id',(req,res)=>{
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body// Parámetros de un cliente
+    res.status(200).json({message: "Delete"})
+})
+
 
 
 // Con esto inicializamos esta app
