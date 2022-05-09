9104f0cf5da61585d322e0b716e7c3f0c8b0867d
= <=>
2022-05-09 12:12:39 -0500

    Paso 6: Agregando Endpoint POST que cree un explorer

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.get('/v1/explorers/:id',(req,res)=>{
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers',(req,res)=>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    //Agregrar logica para persistir
+    const requestBody = req.body// Parámetros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
